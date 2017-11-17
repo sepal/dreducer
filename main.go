9bedc3592b4c12b87f51222242d54617d36e53ef
Sebastian Gilits <sep.gil@gmail.com>
2017-11-17 14:47:40 +0100

    Implement field scanning.

--- main.go
+++ main.go
@@ -7,19 +7,20 @@ import (
 	"github.com/mitchellh/colorstring"
 	"os"
 	"regexp"
+	"strings"
 )
 
 type Field struct {
 	table       string
 	name        string
-	entity_type []Entity
-	bundle      []string
+	entities 	[]string
+	bundles     []string
 }
 
 type Entity struct {
 	table   string
 	bundles []string
-	fields  []Field
+	fields  []string
 }
 
 var fields map[string]Field
@@ -60,12 +61,11 @@ func main() {
 		}
 	}
 
-	for k, entity := range entities {
-		println(k)
-		for _, bundle := range entity.bundles {
-			println("- " + bundle)
-		}
-	}
+	//node, _ := entities["node"]
+	//node.show()
+
+	geocomplete, _ := fields["field_data_field_resume_geocomplete"]
+	geocomplete.show()
 
 	db.Close()
 }
@@ -77,6 +77,13 @@ func processField(db *sql.DB, table string) {
 		printError(err)
 	}
 
+
+	field, exists := fields[table]
+
+	if !exists {
+		field = createField(table)
+	}
+
 	for rows.Next() {
 		var (
 			entity_type string
@@ -85,21 +92,87 @@ func processField(db *sql.DB, table string) {
 
 		rows.Scan(&entity_type, &bundle_name)
 
-		val, exists := entities[entity_type]
+		field.addBundle(bundle_name)
+
+		entity, exists := entities[entity_type]
 
 		if !exists {
-			val = createEntity(entity_type)
+			entity = createEntity(entity_type)
 		}
 
-		val.addBundle(bundle_name)
-		entities[entity_type] = val
+		entity.addBundle(bundle_name)
+		entity.addField(field)
+		field.addEntity(entity)
+
+		entities[entity_type] = entity
 	}
+	fields[table] = field
+}
+
+func createField(field_table string) Field {
+	entities := make([]string, 0)
+	bundles := make([]string, 0)
+
+	name := field_table[11:]
+
+	return Field{table: field_table, name:name, entities:entities, bundles: bundles}
+}
+
+func (field *Field) hasEntity(entity string) bool {
+	for _, v := range field.entities {
+		if v == entity {
+			return true
+		}
+	}
+	return false
+}
+
+func (field *Field) hasBundle(bundle string) bool {
+	for _, v := range field.bundles {
+		if v == bundle {
+			return true
+		}
+	}
+	return false
+}
+
+func (field *Field) addBundle(bundle string) {
+	if !field.hasBundle(bundle) {
+		field.bundles = append(field.bundles, bundle)
+	}
+}
+
+func (field *Field) addEntity(entity Entity) {
+	if !field.hasEntity(entity.table) {
+		field.entities = append(field.entities, entity.table)
+	}
+}
+
+func (field *Field) show() {
+	println(field.name)
+	println("---")
+	println("Belongs to:")
+	for _, entity_name := range field.entities {
+		println("- " + entity_name)
+
+		entity, _ := entities[entity_name]
+
+		padding := strings.Repeat(" ", len(entity_name))
+		for _, bundle := range field.bundles {
+			if entity.hasBundle(bundle) {
+				println("  " + padding + ":" + bundle)
+			}
+		}
+
+	}
+
+
 }
 
 func createEntity(entity_name string) Entity {
 	bundles := make([]string, 0)
-	entity := Entity{table: entity_name, bundles: bundles}
-	return entity
+	fields := make([]string, 0)
+	return Entity{table: entity_name, bundles: bundles, fields: fields}
 }
 
 func (entity *Entity) hasBundle(bundle string) bool {
@@ -114,5 +187,35 @@ func (entity *Entity) hasBundle(bundle string) bool {
 func (entity *Entity) addBundle(bundle string) {
 	if !entity.hasBundle(bundle) {
 		entity.bundles = append(entity.bundles, bundle)
+	}
+}
+
+func (entity *Entity) hasField(field string) bool {
+	for _, v := range entity.fields {
+		if v == field {
+			return true
+		}
+	}
+	return false
+}
+
+func (entity *Entity) addField(field Field) {
+	if !entity.hasField(field.table) {
+		entity.fields = append(entity.fields, field.table)
+	}
+}
+
+func (entity *Entity) show() {
+	println(entity.table)
+	println("---")
+	println("types:")
+	for _, bundle := range entity.bundles {
+		println("- " + bundle)
+	}
+
+	println("fields:")
+	for _, field := range entity.fields {
+		f, _ := fields[field]
+		println("- " + f.name)
 	}
 }
