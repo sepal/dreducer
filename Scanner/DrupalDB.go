d88b85b7e52ea068a7b0f70f0694ca6295b56bd3
Sebastian Gilits <sep.gil@gmail.com>
2017-11-18 13:15:13 +0100

    Refactor db scanning.

---
+++ Scanner/DrupalDB.go
@@ -0,0 +1,119 @@
+package Scanner
+
+import (
+	"github.com/sepal/dreducer/models"
+	"database/sql"
+	"fmt"
+	"regexp"
+)
+
+const FIELD_PATTERN = "(field_data_.+)"
+
+type DrupalDB struct {
+	db *sql.DB
+	name string
+	fields map[string]*models.Field
+	entities map[string]*models.Entity
+}
+
+func CreateDrupalDB(database, host, user, password string) (DrupalDB, error) {
+	drupal := DrupalDB{}
+	dsn := fmt.Sprintf("%s:%s@%s/%s", user, password, host, database)
+
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return drupal, err
+	}
+
+	drupal.db = db
+	drupal.entities = make(map[string]*models.Entity)
+	drupal.fields = make(map[string]*models.Field)
+
+	drupal.scanTables()
+
+	return drupal, nil
+}
+
+func (d *DrupalDB) scanTables() error {
+	r, _ := regexp.Compile(FIELD_PATTERN)
+	rows, err := d.db.Query("SHOW TABLES")
+
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		var table string
+		err := rows.Scan(&table)
+		if err != nil {
+			return err
+		}
+
+		if r.MatchString(table) {
+			d.scanField(table)
+		}
+	}
+
+	return nil
+}
+
+func (d *DrupalDB) scanField(table string) error {
+	rows, err := d.db.Query("SELECT entity_type, bundle FROM " + table)
+
+	if err != nil {
+		return err
+	}
+
+	field, exists := d.fields[table]
+
+	if !exists {
+		f := models.CreateField(table)
+		field = &f
+	}
+
+	for rows.Next() {
+		var (
+			entity_type string
+			bundle_name string
+		)
+
+		err = rows.Scan(&entity_type, &bundle_name)
+
+		if err != nil {
+			return err
+		}
+
+		entity, exists := d.entities[entity_type]
+
+		if !exists {
+			e := models.CreateEntity(entity_type)
+			entity = &e
+		}
+
+		t, exists := entity.GetType(bundle_name)
+
+		if !exists {
+			entity.AddType(bundle_name)
+			t, _ = entity.GetType(bundle_name)
+		}
+
+		field.AddEntityType(t)
+
+		entity.AddField(field, bundle_name)
+
+		d.entities[entity_type] = entity
+	}
+	d.fields[table] = field
+	return nil
+}
+
+func (d *DrupalDB) GetField(name string) (*models.Field, bool) {
+	f, exists := d.fields[name]
+	return f, exists
+}
+
+func (d *DrupalDB) GetEntity(name string) (*models.Entity, bool) {
+	e, exists := d.entities[name]
+	return e, exists
+}
--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
-import "database/sql"
 import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/mitchellh/colorstring"
-	"github.com/sepal/dreducer/models"
 	"os"
-	"regexp"
+	"github.com/sepal/dreducer/Scanner"
 )
-
-var fields map[string]*models.Field
-var entities map[string]*models.Entity
 
 func printError(err error) {
 	fmt.Println(colorstring.Color("[red]" + err.Error()))
@@ -19,85 +14,17 @@ func printError(err error) {
 }
 
 func main() {
-	entities = make(map[string]*models.Entity)
-	fields = make(map[string]*models.Field)
-
-	r, _ := regexp.Compile("(field_data_.+)")
-
-	db, err := sql.Open("mysql", "root:sws232@/drupal")
+	db, err := Scanner.CreateDrupalDB("drupal", "", "root", "sws232")
 
 	if err != nil {
 		printError(err)
 	}
 
-	rows, err := db.Query("SHOW TABLES")
-
-	if err != nil {
-		printError(err)
-	}
-
-	for rows.Next() {
-		var table string
-		err := rows.Scan(&table)
-		if err != nil {
-			printError(err)
-		}
-
-		if r.MatchString(table) {
-			processField(db, table)
-		}
-	}
-
-	node, _ := entities["node"]
-	node.Show()
-
-	geocomplete, _ := fields["field_data_field_resume_geocomplete"]
-	geocomplete.Show()
-
-	db.Close()
-}
-
-func processField(db *sql.DB, table string) {
-	rows, err := db.Query("SELECT entity_type, bundle FROM " + table)
-
-	if err != nil {
-		printError(err)
-	}
-
-	field, exists := fields[table]
-
+	name := "node"
+	entity, exists := db.GetEntity(name)
 	if !exists {
-		f := models.CreateField(table)
-		field = &f
+		fmt.Printf("Entity %s does not exist.\n", name)
+		os.Exit(1)
 	}
-
-	for rows.Next() {
-		var (
-			entity_type string
-			bundle_name string
-		)
-
-		rows.Scan(&entity_type, &bundle_name)
-
-		entity, exists := entities[entity_type]
-
-		if !exists {
-			e := models.CreateEntity(entity_type)
-			entity = &e
-		}
-
-		t, exists := entity.GetType(bundle_name)
-
-		if !exists {
-			entity.AddType(bundle_name)
-			t, _ = entity.GetType(bundle_name)
-		}
-
-		field.AddEntityType(t)
-
-		entity.AddField(field, bundle_name)
-
-		entities[entity_type] = entity
-	}
-	fields[table] = field
+	entity.Show()
 }
