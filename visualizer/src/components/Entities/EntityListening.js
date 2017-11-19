687902362f6976239e9097a31b5f356a3d5095ea
Sebastian Gilits <sep.gil@gmail.com>
2017-11-19 11:12:59 +0100

    Implement listening entity types .

--- visualizer/src/components/Entities/EntityListening.js
+++ visualizer/src/components/Entities/EntityListening.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
+import List from "../List";
 
 const ENTITIES_QUERY = gql`query root { 
   entities {
     id
     name 
+    types {
+      id
+      name
+    }
   }
 }`;
+
 
 function EntityListening(props) {
   if (props.loading) {
@@ -16,9 +22,16 @@ function EntityListening(props) {
     )
   }
 
-  return props.entities.map(entity => <li key={entity.id}>
-    <a href="/">{entity.name}</a>
-  </li>);
+  return props.entities.map((entity) => {
+    const types = entity.types.map(et => <li key={et.id}><a href="">{et.name}</a></li>);
+
+    return (<li key={entity.id}>
+      <a href="/">{entity.name}</a>
+      <List>
+        {types}
+      </List>
+    </li>)
+  });
 }
 
 export default graphql(ENTITIES_QUERY, {
