a4e1963fc55a79a9caa871611542a2258a9d9dd3
Sebastian Gilits <sep.gil@gmail.com>
2017-11-19 10:29:59 +0100

    Add graphql client.

--- visualizer/src/index.js
+++ visualizer/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'
+import {HttpLink, InMemoryCache} from 'apollo-client-preset'
+import {ApolloProvider} from 'react-apollo'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({uri: '/graphql'}),
+  cache: new InMemoryCache().restore({})
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
